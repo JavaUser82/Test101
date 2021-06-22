fc3c82adb42b71d4b772384a3738a9f9ff216d48
revolrution82 <revolrution82@gmail.com>
2021-06-22 14:13:01 +0900

    test0622

---
+++ src/check/TestChk1.java
@@ -0,0 +1,9 @@
+package check;
+
+public class TestChk1 {
+
+	public static void main(String[] args) {
+		System.out.println("테스트  체크 데이터 1=====");
+	}
+
+}
---
+++ src/check/TestChk2.java
@@ -0,0 +1,9 @@
+package check;
+
+public class TestChk2 {
+
+	public static void main(String[] args) {
+		System.out.println("테스트  체크 데이터 2=====");
+	}
+
+}
