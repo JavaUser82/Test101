bc17a4d1ffb3de56a417dc45951c47d70b559764
revolrution82 <revolrution82@gmail.com>
2021-06-22 21:06:15 +0900

    testChk1 수정

--- src/check/TestChk1.java
+++ src/check/TestChk1.java
@@ -3,7 +3,7 @@ package check;
 public class TestChk1 {
 
 	public static void main(String[] args) {
-		System.out.println("테스트  체크 데이터 1=====");
+		System.out.println("테스트  체크 데이터 1=====branch수정");
 	}
 
 }
